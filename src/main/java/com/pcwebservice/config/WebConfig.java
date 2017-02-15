e824ebffb9faccd8c2a7fdfc31a0b94a89c3fe97
leolileo <l.li@campus.tu-berlin.de>
2017-02-15 14:38:52 +0100

    right properties

--- src/main/java/com/pcwebservice/config/WebConfig.java
+++ src/main/java/com/pcwebservice/config/WebConfig.java
@@ -4,14 +4,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import java.security.Principal;
 import java.util.List;
 
 /**
@@ -42,14 +38,13 @@ public class WebConfig extends WebMvcConfigurerAdapter {
                 "/googleRedirect",
                 "/rethink"
         })
-        public ModelAndView redirection(ModelMap model, @ModelAttribute("Principal") Principal Principal) {
+      /*  public ModelAndView redirection(ModelMap model, @ModelAttribute("Principal") Principal Principal) {
             model.addAttribute("Principal", Principal);
             return new ModelAndView("forward:/index.html", model);
-        }
-/*
+        }*/
         public String index() {
             return "forward:/index.html";
         }
-    }*/
     }
+
 }
